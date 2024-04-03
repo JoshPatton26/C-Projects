5209535fd0113cf8ae8eabcd8e94a160012e0788
JoshPatton26 <jdp2@uab.edu>
2024-04-03 12:03:08 -0500

    Adding projects

---
+++ CS332-SystemsProgramming/Lab05/lab05.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/mman.h>
+
+char *filetype(unsigned char type) {
+    char *str;
+    switch(type) {
+        case DT_BLK: str = "block device"; break;
+        case DT_CHR: str = "character device"; break;
+        case DT_DIR: str = "directory"; break;
+        case DT_FIFO: str = "named pipe (FIFO)"; break;
+        case DT_LNK: str = "symbolic link"; break;
+        case DT_REG: str = "regular file"; break;
+        case DT_SOCK: str = "UNIX domain socket"; break;
+        case DT_UNKNOWN: str = "unknown file type"; break;
+        default: str = "UNKNOWN";
+    }
+    return str;
+}
+
+/*
+    Function accepts an unsigned char pointer that points to the given directory path.
+    This will then open and read all contents contained in this directory and prints all
+    files and all sub-directory files contained.
+*/
+void traverseFiles(unsigned char *usrpath){
+    printf("%s\n", usrpath);
+    char path[1000];
+    struct dirent *dp;
+    DIR *dir = opendir(usrpath);
+
+    // Error with opening directory.
+    if(!dir){
+        printf ("Error opening directory '%s'\n", usrpath);
+        exit (-1);
+    }
+
+    // Loop through the files in the given directory and print all directory file and sub-directory files.
+    int count = 1, indcount = 0, i = 0;
+    while((dp = readdir(dir)) != NULL){
+        if(strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0){
+
+            for(i=0; i <= indcount; i++){
+                printf("\t");
+            }
+            // Print files or sub directories and their files.
+            printf("[%d] %s (%s)\n", count, dp->d_name, filetype(dp->d_type));
+
+            char *type = filetype(dp->d_type);
+
+            // Check to see if a sub-directory. If so, form a new path and call the function again.
+            if(strcmp(type, "directory") == 0){
+                //printf("Beginning of IF %d\n", indcount);
+                // Construct a new path from basePath.
+                strcpy(path, usrpath);
+                strcat(path, "/");
+                strcat(path, dp->d_name);
+                indcount--;
+                indcount++;
+                //printf("Before Call %d\n", indcount);
+                traverseFiles(path);
+                
+                //printf("After Call %d\n", indcount);
+            }
+            count++;
+        }
+    }
+    closedir(dir);
+}
+
+int main (int argc, char **argv) {
+    struct dirent *dirent;
+    DIR *parentDir;
+    // Used to assign to argv[1], which is the given name of directory.
+    unsigned char *usrpath;
+    usrpath = malloc(strlen(argv[1]) + 1);
+    strcpy(usrpath, argv[1]);
+
+    // Check to make sure the user entered the command correctly.
+    if (argc < 2) {
+        printf ("Usage: %s <dirname>\n", argv[0]);
+        exit(-1);
+    }
+
+    // Call function to traverse and print all files in given directory and and sub-directories included in given path.
+    traverseFiles(usrpath);
+
+    return 0;
+}
---
+++ CS332-SystemsProgramming/Lab07/README.md
@@ -0,0 +1,7 @@
+This assignment asked us to open a file, read the contents(linux commands) line by line, and execute each of the linux commands that are contained within. This assignment wanted us to use the fork-exec method to operate the process seperately. After this it wanted us to open another file and write what commands were executed and how long it took the computer to execute these commands.
+
+I compiled and ran my code on the Vulcan server and everything ran fine with no warnings or errors.
+
+to compile my code you will need to be on the Vulcan server and you can use the command 'gcc -o lab07 lab07.c', and to run my code you need to use './lab07 (name of test file)' 
+
+I tested mine using a text file which contained 'ls -l', 'ls', and 'echo "random stuff"' all of which ran fine.
